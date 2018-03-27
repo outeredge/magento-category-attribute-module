5b22c40ac91afedbd1aed946e98ab8026910e086
broopdias <ibroopi@gmail.com>
2018-03-27 16:21:44 +0100

    Fixed issue with FileInfo on production mode
    A issue with lazy load / injection occurred on production mode.
    Cannot load file FileInfo
    Argument 1 passed to 
    Magento\Catalog\Model\Category\FileInfo::__construct() must be an instance of Magento\Framework\Filesystem, instance of Magento\Framework\ObjectManager\ObjectManager

--- Plugin/Catalog/Model/Category/DataProvider.php
+++ Plugin/Catalog/Model/Category/DataProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace OuterEdge\CategoryAttribute\Plugin\Catalog\Model\Category;
 
 use Magento\Store\Model\StoreManagerInterface;
@@ -7,16 +6,22 @@ use OuterEdge\CategoryAttribute\Helper\Data as CategoryAttributeHelper;
 use Magento\Catalog\Model\Category\DataProvider as CategoryDataProvider;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\App\ObjectManager;
-use Magento\Catalog\Model\Category\FileInfo;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\File\Mime;
 use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 
 class DataProvider
 {
     /**
+     * Path in /pub/media directory
+     */
+    const ENTITY_MEDIA_PATH = '/catalog/category';
+    
+    /**
      * @var StoreManagerInterface
      */
     private $storeManager;
-
     /**
      * @var CategoryAttributeHelper $categoryAttributeHelper
      */
@@ -25,18 +30,34 @@ class DataProvider
     /**
      * @var Filesystem
      */
-    private $fileInfo;
+    private $filesystem;
 
+    /**
+     * @var Mime
+     */
+    private $mime;
+
+    /**
+     * @var WriteInterface
+     */
+    private $mediaDirectory;
+    
     /**
      * @param StoreManagerInterface $storeManager
      * @param CategoryAttributeHelper $categoryAttributeHelper
+     * @param Filesystem $filesystem
+     * @param Mime $mime
      */
     public function __construct(
         StoreManagerInterface $storeManager,
-        CategoryAttributeHelper $categoryAttributeHelper
+        CategoryAttributeHelper $categoryAttributeHelper,
+        Filesystem $filesystem,
+        Mime $mime
     ) {
         $this->storeManager = $storeManager;
         $this->categoryAttributeHelper = $categoryAttributeHelper;
+        $this->filesystem = $filesystem;
+        $this->mime = $mime;
     }
     
     /**
@@ -50,43 +71,82 @@ class DataProvider
         if (empty($loadedData)) {
             return $loadedData;
         }
-
         $category = $subject->getCurrentCategory();
         if (!$category) {
             return $loadedData;
         }
-
         if (empty($this->categoryAttributeHelper->getCustomImageAttributesAsArray())) {
             return $loadedData;
         }
-
         foreach ($loadedData as $categoryId => $categoryData) {
             foreach ($this->categoryAttributeHelper->getCustomImageAttributesAsArray() as $image) {
                 if (isset($categoryData[$image])) {
                     $fileName = $category->getData($image);
-                    if ($this->getFileInfo()->isExist($fileName)) {
-                        $stat = $this->getFileInfo()->getStat($fileName);
-                        $mime = $this->getFileInfo()->getMimeType($fileName);
+                    if ($this->isExist($fileName)) {
+                        $stat = $this->getStat($fileName);
+                        $mime = $this->getMimeType($fileName);
                         $loadedData[$categoryId][$image][0]['size'] = isset($stat) ? $stat['size'] : 0;
                         $loadedData[$categoryId][$image][0]['type'] = $mime;
                     }
                 }
             }
         }
-
         return $loadedData;
     }
     
-    /**
-     * Get FileInfo instance
+     /**
+     * Get WriteInterface instance
      *
-     * @return FileInfo
+     * @return WriteInterface
      */
-    private function getFileInfo()
+    private function getMediaDirectory()
     {
-        if ($this->fileInfo === null) {
-            $this->fileInfo = ObjectManager::getInstance()->get(FileInfo::class);
+        if ($this->mediaDirectory === null) {
+            $this->mediaDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA);
         }
-        return $this->fileInfo;
+        return $this->mediaDirectory;
+    }
+
+    /**
+     * Retrieve MIME type of requested file
+     *
+     * @param string $fileName
+     * @return string
+     */
+    public function getMimeType($fileName)
+    {
+        $filePath = self::ENTITY_MEDIA_PATH . '/' . ltrim($fileName, '/');
+        $absoluteFilePath = $this->getMediaDirectory()->getAbsolutePath($filePath);
+
+        $result = $this->mime->getMimeType($absoluteFilePath);
+        return $result;
+    }
+
+    /**
+     * Get file statistics data
+     *
+     * @param string $fileName
+     * @return array
+     */
+    public function getStat($fileName)
+    {
+        $filePath = self::ENTITY_MEDIA_PATH . '/' . ltrim($fileName, '/');
+
+        $result = $this->getMediaDirectory()->stat($filePath);
+        return $result;
+    }
+
+    /**
+     * Check if the file exists
+     *
+     * @param string $fileName
+     * @return bool
+     */
+    public function isExist($fileName)
+    {
+        $filePath = self::ENTITY_MEDIA_PATH . '/' . ltrim($fileName, '/');
+
+        $result = $this->getMediaDirectory()->isExist($filePath);
+        return $result;
     }
 }
